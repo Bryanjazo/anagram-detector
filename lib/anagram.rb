2840680e967d85b8120ee5aa4c98bb8b95c39b95
Bryanjazo <bryanjazo88@gmail.com>
2021-02-09 16:21:09 -0800

    commit tree 6f9b2cf26aad60cb01eb30011f8128d3d66bed78

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :analyzed_words
+
+  def initialize(analyzed_words)
+    @analyzed_words = analyzed_words
+  end
+  def match(find_string)
+    puts "Must find #{self.analyzed_words} inside the following string: #{find_string}"
+    find_string.find_all do |word|
+    if word.split("").sort == self.analyzed_words.split("").sort
+      word
+    end
+    end
+  end
+end
